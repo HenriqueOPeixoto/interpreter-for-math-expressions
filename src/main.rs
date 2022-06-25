6dacb26e1476fe2f7a1f4e6639aec4b8edfe563e
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-06-24 21:55:47 -0400

    read input from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+
+    println!("{}", filename);
+
+    let input_file = fs::read_to_string(filename)
+        .expect("Algo deu errado ao ler o arquivo!");
+
+    println!("{}", input_file);
+    
+
 }
